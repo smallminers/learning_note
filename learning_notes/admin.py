0114da429c073362575cd34b1520dc52b1b17701
smallminers <1129185991@qq.com>
2018-09-02 20:55:47 +0800

    迁移模型 Entry
    由于我们添加了一个新模型，因此需要再次迁移数据库。你将慢慢地对这个过程了如指掌：修改 models.py ，执行命令 python manage.py makemigrations app_name ，
    再执行命令 python manage.py migrate 。

--- learning_notes/admin.py
+++ learning_notes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from learning_notes.models import Topic
+
+admin.site.register(Topic)
--- learning_notes/models.py
+++ learning_notes/models.py
@@ -12,18 +12,18 @@ class Topic(models.Model):
         """返回模型的字符串表示"""
         return self.text
 
+
 class Entry(models.Model):
-    """学到有关某个主题的具体知识"""
-    topic = models.ForeignKey(Topic)
+    """学到关于特定主题的具体知识"""
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        verbose_name_plural = 'entries'
+class Meta:
+    verbose_name_plural = 'entries'
 
-    def __str__(self):
-        """返回模型的字符串表示"""
-
-        return self.text[:50] + '...'
+def __str__(self):
+    """返回模型的字符串表示"""
+    return self.text[:50] + "..."
 
 
